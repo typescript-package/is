bd8623e3defa688da80f68c66bc60175e610ddde
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-12-02 13:18:45 +0000

    chore(api): update.

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,48 +1,54 @@
 /*
  * Public API Surface of is
  */
-// `is` functions.
-export { isArray } from './lib/is-array.func';
-export { isBigInt } from './lib/is-big-int.func';
-export { isBoolean } from './lib/is-boolean.func';
-export { isBooleanObject } from './lib/is-boolean-object.func';
-export { isBooleanType } from './lib/is-boolean-type.func';
-export { isClass } from './lib/is-class.func';
-export { isDate } from './lib/is-date.func';
-export { isDefined } from './lib/is-defined.func';
-export { isFalse } from './lib/is-false.func';
-export { isFunction } from './lib/is-function.func';
-export { isInstance } from './lib/is-instance.func';
-export { isKey } from './lib/is-key.func';
-export { isNull } from './lib/is-null.func';
-export { isNumber } from './lib/is-number.func';
-export { isNumberBetween } from './lib/is-number-between.func';
-export { isNumberObject } from './lib/is-number-object.func';
-export { isNumberType } from './lib/is-number-type.func';
-export { isObject } from './lib/is-object.func';
-export { isObjectKey } from './lib/is-object-key.func';
-export { isObjectKeyIn } from './lib/is-object-key-in.func';
-export { isObjectKeys } from './lib/is-object-keys.func';
-export { isObjectKeysIn } from './lib/is-object-keys-in.func';
-export { isObjectSomeKeys } from './lib/is-object-some-keys.func';
-export { isParam } from './lib/is-param.decorator';
-export { isPrimitive } from './lib/is-primitive.func';
-export { isRegExp } from './lib/is-regexp.func';
-export { isString } from './lib/is-string.func';
-export { isStringIncludes } from './lib/is-string-includes.func';
-export { isStringIncludesSome } from './lib/is-string-includes-some.func';
-export { isStringLength } from './lib/is-string-length.func';
-export { isStringLengthBetween } from './lib/is-string-length-between.func';
-export { isStringObject } from './lib/is-string-object.func';
-export { isStringType } from './lib/is-string-type.func';
-export { isSymbol } from './lib/is-symbol.func';
-export { isTrue } from './lib/is-true.func';
-export { isType } from './lib/is-type.func';
-export { isUndefined } from './lib/is-undefined.func';
-// `is` object.
-export { is } from './lib/is.object';
-// All `isNot` functions.
 export {
+  // `is` object.
+  is,
+
+  // `is` functions.
+  isArray,
+  isBigInt,
+  isBoolean,
+  isBooleanObject,
+  isBooleanType,
+  isClass,
+  isDate,
+  isDefined,
+  isFalse,
+  isFunction,
+  isInstance,
+  isKey,
+  isNull,
+  isNumber,
+  isNumberBetween,
+  isNumberObject,
+  isNumberType,
+  isObject,
+  isObjectKey,
+  isObjectKeyIn,
+  isObjectKeys,
+  isObjectKeysIn,
+  isObjectSomeKeys,
+  isParam,
+  isPrimitive,
+  isRegExp,
+  isString,
+  isStringIncludes,
+  isStringIncludesSome,
+  isStringLength,
+  isStringLengthBetween,
+  isStringObject,
+  isStringType,
+  isSymbol,
+  isTrue,
+  isType,
+  isUndefined
+} from './lib';
+export {
+  // `isNot` object.
+  isNot,
+
+  // All `isNot` functions.
   isNotBoolean,
   isNotDefined,
   isNotFunction,
@@ -51,5 +57,3 @@ export {
   isNotString,
   isNotUndefined
 } from './not';
-// `isNot`.
-export { isNot } from './not';
